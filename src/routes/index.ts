3b4b57a0dc0898b0673a6d834ba5beba15a4daaf
Mariano Benedettini <mbenedettini@gmail.com>
2018-10-13 17:07:05 -0300

    Edit user
    Also: validate uuid coming from request

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,12 @@ const EMAIL_REGEXP = new RegExp(
   /(([a-zA-Z][^<>()\[\]\\.\/,;:\s@"=]+(\.[^<>()\[\]\\.\/,;:\s@="]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/,
 )
 
+function isValidUUID(value: string): boolean {
+  return !!value.match(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+  )
+}
+
 router.get('/', async function(req: any, res: any, next: any) {
   const repo = getManager().getRepository(User)
   const users = await repo.find()
@@ -38,6 +44,59 @@ router.post('/new', async function(req: any, res: any, next: any) {
       error: 'Email address not valid',
     })
   }
+})
+
+router.get('/:id', async (req: any, res: any, next: any) => {
+  if (!isValidUUID(req.params.id)) {
+    return res.render('edit', {
+      user: {},
+      error: 'Invalid user id',
+    })
+  }
+  const repo = getManager().getRepository(User)
+  const user = await repo.findOne(req.params.id)
+  if (!user) {
+    return res.render('edit', {
+      user: {},
+      error: 'User not found',
+    })
+  }
+  res.render('edit', {
+    user: user,
+  })
+})
+
+router.post('/:id', async (req: any, res: any, next: any) => {
+  if (!isValidUUID(req.params.id)) {
+    return res.render('edit', {
+      user: {},
+      error: 'Invalid user id',
+    })
+  }
+  const repo = getManager().getRepository(User)
+  let user = await repo.findOne(req.params.id)
+  if (!user) {
+    return res.render('edit', {
+      user: {},
+      error: 'User not found',
+    })
+  }
+  const email = req.body.email
+  user.email = email
+  try {
+    log.debug('Updating user...')
+    await repo.save(user)
+  } catch (e) {
+    log.error(e)
+    return res.render('edit', {
+      user: user,
+      error: 'Some error has happened',
+    })
+  }
+  res.render('edit', {
+    user: user,
+    message: 'User successfully saved',
+  })
 })
 
 export { router as index }
