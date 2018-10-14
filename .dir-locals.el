49135da34437fa95db96ba89865b8e4d41eaaf91
Mariano Benedettini <mbenedettini@gmail.com>
2018-10-14 17:19:17 -0300

    Include Emacs locals

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js2-mode
+  (js2-basic-offset . 2))
+ (typescript-mode
+  (typescript-indent-level . 2)))
+
