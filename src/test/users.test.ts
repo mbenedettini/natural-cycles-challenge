d33dc479ecaca189c362d60d2af5fe712c6b6310
Mariano Benedettini <mbenedettini@gmail.com>
2018-10-14 15:37:30 -0300

    List users tests

--- src/test/users.test.ts
+++ src/test/users.test.ts
@@ -1,13 +1,37 @@
-import log from '../../src/logger'
+const request = require('request-promise')
 
-beforeAll(() => {
-  log.debug('beforeAll')
-})
-
-afterAll(() => {
-  log.debug('afterAll')
-})
+import log from '../logger'
+import { loadUser } from './fixtures'
+import { setupTests, getBaseURL } from './utils'
+setupTests()
 
 test('Placeholder', () => {
   expect(1).toBe(1)
+})
+
+test('List users', async () => {
+  const data = await request({
+    uri: getBaseURL() + '/users',
+    json: true,
+  })
+  log.debug(data)
+  expect(data).toEqual({
+    data: [],
+  })
+
+  const user = await loadUser()
+
+  const dataNotEmpty = await request({
+    uri: getBaseURL() + '/users',
+    json: true,
+  })
+  log.debug(dataNotEmpty)
+  expect(dataNotEmpty).toEqual({
+    data: [
+      {
+        id: user.id,
+        email: user.email,
+      },
+    ],
+  })
 })
